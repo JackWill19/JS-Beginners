0a688af530e4cc3bf83d2fdd99e77a12ca5549f5
Jack Williams <jackw@orphans.co.uk>
2022-11-22 15:25:02 +0000

    Dom Basics

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,8 +1,20 @@
-const paragraph = document.querySelector('p'); // Selecting one paragraph element
-const divs = document.querySelectorAll('div'); // Selecting all divs
-const img = document.querySelectorAll('.item img'); // Selecting a specific element inside a div
+// const paragraph = document.querySelector('p'); // Selecting one paragraph element
+// const divs = document.querySelectorAll('div'); // Selecting all divs
+// const img = document.querySelectorAll('.item img'); // Selecting a specific element inside a div
+// const heading = document.querySelector('h2');
 
-console.log('paragraph');
-console.log('divs');
-console.log('img');
+// console.log('paragraph');
+// console.log('divs');
+// console.log('img');
 
+// console.dir('heading'); // console.dir - Shows object properties 
+// heading.textContent = 'Jack is cool'; // Sets the content of the object(SETTING)
+// console.log(heading.textContent);// .textContent shows the content of the object (GETTING)... innerText does the same but is aware of CSS styling
+
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+// pizzaList.textContent = `${pizzaList.textContent} 17!`;
+pizzaList.insertAdjacentText('beforeend', '17!'); //Easiest way to add text to existing text
+console.log(pizzaList);

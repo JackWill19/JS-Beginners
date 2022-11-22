77b7968776cbacfd3e2d7ecd814edad4c7cb565a
Jack Williams <jackw@orphans.co.uk>
2022-11-22 14:54:56 +0000

    The dom basics

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,8 @@
+const paragraph = document.querySelector('p'); // Selecting one paragraph element
+const divs = document.querySelectorAll('div'); // Selecting all divs
+const img = document.querySelectorAll('.item img'); // Selecting a specific element inside a div
+
+console.log('paragraph');
+console.log('divs');
+console.log('img');
+

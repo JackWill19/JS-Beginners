d91f8230d11cb8f66802a740ade8fb502055fafc
Jack Williams <jackw@orphans.co.uk>
2022-11-24 16:52:01 +0000

    Tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,23 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(e) {
+    //Hide panels
+    tabPanels.forEach(panel =>{
+        panel.hidden = true;
+    });
+    //mark tabs unselected
+    tabButtons.forEach(tab => {
+        tab.setAttribute('aria-selected', false);
+    });
+    //mark tab as selected
+    e.currentTarget.setAttribute('aria-selected', true);
+
+    //find associated tab panel and show
+    const { id } = e.currentTarget;
+    const tabPanel = tabPanels.find(panel => panel.getAttribute('aria-labelledby') === id);
+    tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));

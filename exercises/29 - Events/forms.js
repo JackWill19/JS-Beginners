6e5a32fc10c17e9c48dbf90cda4dad2e80782878
Jack Williams <jackw@orphans.co.uk>
2022-11-24 09:53:04 +0000

    Forms

--- exercises/29 - Events/forms.js
+++ exercises/29 - Events/forms.js
@@ -27,3 +27,13 @@ signupForm.name.addEventListener('keyup', logEvent);
 signupForm.name.addEventListener('keydown', logEvent);
 signupForm.name.addEventListener('focus', logEvent);
 signupForm.name.addEventListener('blur', logEvent);
+
+function handlePhotoClick(e){
+    if(e.type === 'click' || e.key === 'Enter'){
+    console.log('Clicked!');
+    }
+}
+
+const photo = document.querySelector('.photo');
+photo.addEventListener('click', handlePhotoClick);
+photo.addEventListener('keyup', handlePhotoClick);
